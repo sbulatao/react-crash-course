809c83fe1cabca560d5ffea636787010e069c24c
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-21 20:30:18 -0700

    API Integration part2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-
 function Home() {
     const [users, setUsers] = useState([])
 
     async function fetchUsers(){
         const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
         setUsers(data);
+        console.log(data);
     }
 
     useEffect(() => {
         setTimeout(() => {
             fetchUsers();
-        }, 2000);
+        }, 500);
     }, []);
+
+    const pixels = "3px"; 
 
     return (
         <div>
-        {users.length > 0
-        ? <h1>{users[0]?.name}</h1>
-        : <h1>Loading...</h1>
-        }
+        {users.map((user) => (
+            <div key={user.id} style={{ border: `${pixels} solid black`}}>
+                <div>{user?.id}</div>
+                <div>{user?.name}</div>
+                <div>{user?.email}</div>
+                <div>{user?.username}</div>
+            </div>
+        ))}
         </div>
     );
 }
