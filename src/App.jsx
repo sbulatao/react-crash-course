529c35b91c4530e1cace0dc00c1ba249d011f550
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-21 19:22:47 -0700

    React Routing part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
-import Todo from './components/Todo.jsx' // for Todo.jsx
-import Title from './components/Title.jsx'; // for Title.jsx
-import Modal from './components/Modal.jsx'; // for Modal.jsx
-import Counter from './components/Counter.jsx'; // for Counter.jsx
-
-import React, { useState, useEffect, use } from 'react';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-
-  function onTodoDelete(){
-    setShowModal(true)
-    // console.log('onTodoDelete()')
-  }
-
-  function modalCancel(){
-    setShowModal(false)
-    console.log('modalCancel()')
-  }
-
-  function modalConfirm(){
-    setShowModal(false)
-    console.log('modalConfirm()')
-  }
-
-  useEffect(() => {
-    console.log('ONLY on mount')
-  }, [])
-
-  useEffect(() => {
-    console.log('on mount AND on ${showModal} change')
-    // setShowModal(!showModal) // inf loop might break.... glitchy
-  }, [showModal])
-
-  useEffect(() => {
-    console.log('EVERY render')
-  })
-
-  return (
+  return(
     <div>
-      <Title />
-      <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className='todo__wrapper'>
-      <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified"/>
-      <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section"/>
-      <Todo onTodoDelete={onTodoDelete} title="Land a Job"/>
-      </div>
-      {showModal && (
-        <Modal 
-          modalCancel={modalCancel} 
-          modalConfirm={modalConfirm} 
-          title="Confirm Delete?"/>
-      )}
+      <Router>
+        <nav>
+          {/* Quick Navigations */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          {/* <a href="/">Home</a> */}
+          {/* <a href="/About">About</a> */}
+          {/* <a href="/Contact">Contact</a> */}
+        </nav>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
+}
+
+export default App;
 
 
+////////////////////////////////////////////////////////////////////////////
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Todo from './components/Todo.jsx' // for Todo.jsx
+// import Title from './components/Title.jsx'; // for Title.jsx
+// import Modal from './components/Modal.jsx'; // for Modal.jsx
+// import Counter from './components/Counter.jsx'; // for Counter.jsx
+
+// import React, { useState, useEffect, use } from 'react';
+
+
+// function App() {
+//   const [showModal, setShowModal] = useState(false)
+
+//   function onTodoDelete(){
+//     setShowModal(true)
+//     // console.log('onTodoDelete()')
+//   }
+
+//   function modalCancel(){
+//     setShowModal(false)
+//     console.log('modalCancel()')
+//   }
+
+//   function modalConfirm(){
+//     setShowModal(false)
+//     console.log('modalConfirm()')
+//   }
+
+//   useEffect(() => {
+//     console.log('ONLY on mount')
+//   }, [])
+
+//   useEffect(() => {
+//     console.log('on mount AND on ${showModal} change')
+//     // setShowModal(!showModal) // inf loop might break.... glitchy
+//   }, [showModal])
+
+//   useEffect(() => {
+//     console.log('EVERY render')
+//   })
+
+//   return (
+//     <div>
+//       <Title />
+//       <div>
+//         <input type="text" onChange={(event) => {
+//           console.log(event.target.value)
+//         }} />
+//         <button onClick={() => setShowModal(true)}>Add todo</button>
+//       </div>
+//       <div className='todo__wrapper'>
+//       <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified"/>
+//       <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section"/>
+//       <Todo onTodoDelete={onTodoDelete} title="Land a Job"/>
+//       </div>
+//       {showModal && (
+//         <Modal 
+//           modalCancel={modalCancel} 
+//           modalConfirm={modalConfirm} 
+//           title="Confirm Delete?"/>
+//       )}
+//     </div>
+//   );
+
+//////////////////////////////////////////////////////////////////////////
 
     /**
    * 1. Create a "Counter.jsx" component
@@ -112,6 +146,6 @@ function App() {
   //     {showModal && <Modal title="Confirm Delete?"/>}
   //   </div>
   // );
-}
+// }
 
-export default App;
+// export default App;
